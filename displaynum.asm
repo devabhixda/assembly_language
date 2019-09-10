d7d667df80571436f4ed039cea0860e9fb7dcbaf
devabhixda <devabhixda@gmail.com>
2019-09-10 18:52:31 +0530

    Add ability to display the scanned number
    Signed-off-by: devabhixda <devabhixda@gmail.com>

--- displaynum.asm
+++ displaynum.asm
@@ -1,17 +1,24 @@
 section .bss
-	digit resb 1
+	buffer resb 64
+	buffer_len resq 1
+
 section .text
 global _start
 _start:
-	add rax,51
-	mov [digit],al
-	mov rax,1
-	mov rdi,1
-	mov rsi,digit
-	mov rdx,2
+	mov rax, 0
+	mov rdi, 0
+	mov rsi, buffer
+	mov rdx,64
 	syscall
 
-        mov rax, 60
-        mov rdi, 0
-        syscall
+	mov [buffer_len],rax
 
+	mov rax,1
+	mov rdi,1
+	mov rsi,buffer
+	mov rdx, [buffer_len]
+	syscall
+
+	mov rax, 60 ;Exit
+	mov rdi, 0
+	syscall
